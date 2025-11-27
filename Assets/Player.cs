c00764e5954d052149e0ca682e9830059f5e48a3
Jingyi Yan <yyyyjjjjjyyyyy6688@gmail.com>
2025-11-27 14:02:02 +0100

    health bar

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,31 +5,87 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-	public int maxHealth = 100;
-	public int currentHealth;
+    public int maxHealth = 100;
+    public int currentHealth;
+    
 
-	public HealthBar healthBar;
+    public TrackGenreReader trackReader;
+    public float energy = 0f;
 
-    // Start is called before the first frame update
+    public HealthBar healthBar;
+
     void Start()
     {
-		currentHealth = maxHealth;
-		healthBar.SetMaxHealth(maxHealth);
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
-    // Update is called once per frame
     void Update()
     {
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			TakeDamage(20);
-		}
+        if (trackReader.CurrentAudioFeatures != null)
+        {
+            energy = trackReader.CurrentAudioFeatures.energy;
+        }
+
+
+        AutoRegenHealth();
     }
 
-	void TakeDamage(int damage)
-	{
-		currentHealth -= damage;
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
 
-		healthBar.SetHealth(currentHealth);
-	}
+        healthBar.SetHealth(currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Player Died");
+        GameManager.Instance?.EndGame();
+    }
+
+    float recoverHealthSpeed(float energy)
+    {
+        if (energy < 0.7f)
+        {
+            return 1f + 1f * energy;      // 3 ~ 6.5
+        }
+        else
+        {
+            return 1.5f + 1.5f * energy;      // 8.5 ~ 10
+        }
+    }
+
+    void AutoRegenHealth()
+    {
+       
+        if (currentHealth >= maxHealth)
+            return;
+
+        float recoverRate = recoverHealthSpeed(energy);
+
+    
+        float regenAmount = energy * recoverRate * Time.deltaTime;
+
+        currentHealth += Mathf.RoundToInt(regenAmount);
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        healthBar.SetHealth(currentHealth);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            TakeDamage(1);
+        }
+    }
+
+
 }
